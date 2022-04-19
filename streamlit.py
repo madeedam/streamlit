60ba9bf51f604a69ee79842123ce828ba9af9a3e
madeedam <101204246+madeedam@users.noreply.github.com>
2022-04-19 22:37:54 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,99 +1,107 @@
-from re import L
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
 import datetime
+import calendar
+import os
+import xlsxwriter
+from io import BytesIO
 
-st.set_page_config(
-    layout="centered", page_icon="📝", page_title="Inventory Stock Control", initial_sidebar_state='collapsed'
-)
-# Bye Bye Menu
-# st.markdown(""" <style>
-# #MainMenu {visibility: hidden;}
-# footer {visibility: hidden;}
-# </style> """, unsafe_allow_html=True)
+st.set_page_config(layout="wide")
+root = os.path.join(os.path.dirname(__file__))
 
-# Session Key
-if 'last_updated' not in st.session_state:
-    st.session_state.last_updated = '-'
+st.markdown(f'''
+    <style>
+        .stDownloadButton>button {{
+            background-color: #f44336;
+            color: white;
+            height: 3rem;
+            width: 100%;
+        }}
+    </style>''', unsafe_allow_html=True)
 
-@st.cache
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv(sep=';').encode('utf-8')
+def getEntries(timestamps):
+    ValidEntries = []
+    for item in timestamps:
+        entry, leave = '', ''
+        if len(item) == 0:
+            entry = ''
+            leave = ''
 
-def stripData(dataframe):
-    tempdf = dataframe.copy()
-    tempdf.columns = tempdf.columns.str.strip()
+        # CutOff 2 PM
+        elif len(item) == 1:
+            if int(item[0].split(':')[0]) < 14:
+                entry = item[0]
+            else:
+                leave = item[0]
 
-    df_obj = tempdf.select_dtypes(['object'])
-    tempdf[df_obj.columns] = df_obj.apply(lambda x: x.str.replace("'",'').str.strip())
-    
-    return tempdf
+        elif len(item) == 2:
+            entry = item[0]
+            leave = item[1]
 
-st.title("📝 L'Oreal Inventory")
+        else:
+            entry = item[0] # First Fingerprint = First Entry
+            for x in item[1:]:
+                if int(x.split(':')[0]) >= 14: # First Fingerprint after 2PM = Leave
+                    leave = x
+                    break
 
-@st.cache(allow_output_mutation=True)
-def getInventory(url):
-    tempdf = stripData(pd.read_csv(url, delimiter=';', dtype=str))
-    tempdf['QTY STOK UPDATE(PCS)'] = tempdf['QTY STOK UPDATE(PCS)'].apply(lambda x: int(x.replace('.','')))
-    tempdf['ISI/LSN'] = tempdf['ISI/LSN'].astype(int)
-    tempdf['SCH-RCNG'] = tempdf['NAMA BARANG'].str.contains('SCH-R')
-    tempdf['PCODE-SCYLLA'] = tempdf['KODE SCYLLA'] + ' | ' + tempdf['NAMA BARANG']
-    tempdf['PCODE-DMS'] = tempdf['KODE DMS'] + ' | ' + tempdf['NAMA BARANG']
-    st.session_state.last_updated = datetime.datetime.now().strftime('%B %d ,%Y - %H:%M:%S')
-    return tempdf, st.session_state.last_updated
+        payload = [entry, leave]
+        ValidEntries.append(payload)
 
-INV, last_updated = getInventory('https://raw.githubusercontent.com/madeedam/streamlit/main/INV.csv')
-st.success(f"Last updated: {last_updated}")
+    return ValidEntries
 
-pcode_choice = st.selectbox('Tipe PCODE', ['DMS','SCYLLA'])
+def Office_K2(uploadedFile, daysinmonth):
+    data = pd.read_excel(uploadedFile, engine='xlrd', sheet_name='Catatan')
+    col = [('','ID'),('','NAME')]
+    for day in daysinmonth:
+        col.append((day,'Datang'))
+        col.append((day,'Pulang'))
 
-if pcode_choice == 'DMS':
-    selection = st.selectbox("Product Name", INV['PCODE-DMS'])
-elif pcode_choice == 'SCYLLA':
-    selection = st.selectbox("Product Name", INV['PCODE-SCYLLA'])
+    DF = pd.DataFrame(columns=pd.MultiIndex.from_tuples(col))
+    for index in range(3,len(data),2):
+        EID = str(data.at[index,'Unnamed: 2']).strip()
+        ENAME = str(data.at[index,'Unnamed: 10']).strip()
 
-ProdName = [x.strip() for x in selection.split('|')][1]
-ItemInfo = INV[INV['NAMA BARANG'] == ProdName]
+        if ENAME == 'nan':
+            ENAME = ''
 
-CartonConversion = ItemInfo['ISI/LSN'].values[0]
-TotalPCS, Carton, Pieces = ItemInfo['QTY STOK UPDATE(PCS)'].values[0],0,0
-if ItemInfo['SCH-RCNG'].values == True:
-    tempvalue = ItemInfo['QTY STOK UPDATE(PCS)'].values[0] / 8
-    Carton = int(tempvalue // CartonConversion)
-    Pieces = int(tempvalue % CartonConversion)
-else:
-    Carton = int(TotalPCS // CartonConversion)
-    Pieces = int(TotalPCS % CartonConversion)
+        try:
+            timestamp = data.iloc[index+1].values
+        except IndexError:
+            timestamp = np.empty(shape=(1,31), dtype=object).flatten()
 
-st.text_input("Stock Awal", value=f"{str(Carton).zfill(3)}.000.{str(Pieces).zfill(3)} <=> {TotalPCS} pcs", disabled=True)
-
-with st.expander("Adjustment"):
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        CM = st.number_input("Carton", min_value=1, max_value=Carton)
-        OpenCarton = Carton - CM
-    with col2:
-        st.number_input("Pieces", value=(OpenCarton * CartonConversion) + Pieces)
-    with col3:
-        st.markdown("Save Adjustment")
-        st.button("Confirm")
-
-# with st.expander("Selisih"):
-#     with st.form("StockManual", clear_on_submit=True):
-#         col1, col2, col3 = st.columns(3)
-#         with col1:
-#             st.number_input("Carton", value=0)
-#         with col2:
-#             st.number_input("Pieces", value=0)
-#         with col3:
-#             st.number_input("Total Pieces", value=0)
-
-#         if st.form_submit_button("Confirm"):
-#             st.success("Data telah disimpan.")
+        checklog = []
+        for line in timestamp:
+            checklog.append(str(line).split('\n')[:-1])
+        checklog = checklog[:len(daysinmonth)]
         
+        VE = getEntries(checklog)
+        DF.at[len(DF)] = [EID, ENAME, *[item for sublist in VE for item in sublist]]
+    
+    return DF
 
-# Download file
-# df = convert_df(INV)
-# if st.sidebar.download_button("Download File", data=df, file_name='INV.csv', mime='text/csv'):
-#     st.sidebar.info("Downloading")
+def convertFingerprint():
+    with st.sidebar:
+        st.subheader("Fingerprint Conversion")
+        uploadedFile = st.file_uploader("Upload File")
+        officeSelection = st.radio('Select Office', ['Mataram','Jegles'])
+        DIM = int(st.number_input("Select Month", value=datetime.datetime.now().month, min_value=1, max_value=12))
+
+    daysinmonth = [datetime.date(2022, DIM, day).strftime('%d/%m/%Y') for day in range(1, calendar.monthrange(2022, DIM)[1]+1)]
+    if officeSelection == 'Jegles' and uploadedFile:
+        DF = Office_K2(uploadedFile, daysinmonth)
+        st.dataframe(DF, height=800)
+
+    if uploadedFile:
+        output = BytesIO()
+        DF.to_excel(output)
+        with st.sidebar:
+            st.download_button(
+            label="Download Excel workbook",
+            data=output.getvalue(),
+            file_name="workbook.xlsx",
+            mime="application/vnd.ms-excel"
+        )
+    
+convertFingerprint()
